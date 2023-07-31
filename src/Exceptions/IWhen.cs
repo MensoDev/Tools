85abb065520cb1f5be3e5e66713426750570fe69
Emerson Trindade (MensoDev) <emerson.trindade@codeblox.com.br>
2023-07-31 15:20:55 -0300

    Add [NotNull] attribute to exception methods
    The [NotNull] attribute was added to the argument inputs of the exception methods in the IWhen interface, When_GuidType, When_EnumerableType, and When_StringType classes, forcing callers to provide a non-null argument. This change highlights the requirement for a non-null argument more explicitly and helps the prevention of null reference errors. Removed redundant spaces in Menso.Tools.Exceptions.csproj.

--- src/Exceptions/IWhen.cs
+++ src/Exceptions/IWhen.cs
@@ -14,22 +14,22 @@ public interface IWhen
     #region String type
 
     void Empty(string argument, string? message = null, Exception? innerException = null, [CallerArgumentExpression(nameof(argument))] string? paramName = null);
-    void EmptyOrNull(string? argument, string? message = null, Exception? innerException = null, [CallerArgumentExpression(nameof(argument))] string? paramName = null);
-    void NullOrWhiteSpace(string? argument, string? message = null, Exception? innerException = null, [CallerArgumentExpression(nameof(argument))] string? paramName = null);
+    void EmptyOrNull([NotNull] string? argument, string? message = null, Exception? innerException = null, [CallerArgumentExpression(nameof(argument))] string? paramName = null);
+    void NullOrWhiteSpace([NotNull] string? argument, string? message = null, Exception? innerException = null, [CallerArgumentExpression(nameof(argument))] string? paramName = null);
 
     #endregion
 
     #region Enumerable type
 
     void Empty<T>(IEnumerable<T> argument, string? message = null, Exception? innerException = null, [CallerArgumentExpression(nameof(argument))] string? paramName = null);
-    void EmptyOrNull<T>(IEnumerable<T>? argument, string? message = null, Exception? innerException = null, [CallerArgumentExpression(nameof(argument))] string? paramName = null);
+    void EmptyOrNull<T>([NotNull] IEnumerable<T>? argument, string? message = null, Exception? innerException = null, [CallerArgumentExpression(nameof(argument))] string? paramName = null);
 
     #endregion
 
     #region Guid type
 
     void Empty(Guid argument, string? message = null, Exception? innerException = null, [CallerArgumentExpression(nameof(argument))] string? paramName = null);
-    void EmptyOrNull(Guid? argument, string? message = null, Exception? innerException = null, [CallerArgumentExpression(nameof(argument))] string? paramName = null);
+    void EmptyOrNull([NotNull] Guid? argument, string? message = null, Exception? innerException = null, [CallerArgumentExpression(nameof(argument))] string? paramName = null);
 
     #endregion
 
--- src/Exceptions/When_StringType.cs
+++ src/Exceptions/When_StringType.cs
@@ -12,13 +12,13 @@ internal partial class When
             ThrowException($"Argument '{paramName}' must not be empty", message, paramName, innerException);
     }
 
-    public void EmptyOrNull(string? argument, string? message = null, Exception? innerException = null, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+    public void EmptyOrNull([NotNull] string? argument, string? message = null, Exception? innerException = null, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
     {
         if (string.IsNullOrEmpty(argument))
             ThrowException($"Argument '{paramName}' must not be null or empty", message, paramName, innerException);
     }
 
-    public void NullOrWhiteSpace(string? argument, string? message = null, Exception? innerException = null, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+    public void NullOrWhiteSpace([NotNull] string? argument, string? message = null, Exception? innerException = null, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
     {
         if (string.IsNullOrWhiteSpace(argument))
             ThrowException($"Argument '{paramName}' must not be null or white space", message, paramName, innerException);
